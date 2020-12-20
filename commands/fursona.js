697b4542d955456d61cf1389131b41c7869ea758
Dorival Neto <dorivaltn@gmail.com>
2020-12-20 00:13:46 -0500

    Moving some text-constants to language files

--- commands/fursona.js
+++ commands/fursona.js
@@ -1,11 +1,10 @@
-const { SPECIES, COLORS } = require("../shared/constants");
 const contentGlobal = require("../language/en/global.json");
 const content = require("../language/en/fursona.json");
 const {
+  sampleItemsFromArray,
   randomItem,
   randomBetween,
   tryWin,
-  sampleItemsFromArray,
   pad
 } = require("../shared/utils");
 
@@ -15,7 +14,8 @@ module.exports = {
   execute(msg, args) {
 
     let name = `${randomItem(content.namePrepend)}-${randomItem(content.bodyPart)}`;
-    let title = randomItem(content.title)
+    let title = randomItem(content.title);
+    const isHybrid = tryWin(10);
 
     let fursonaSpecies = '';
     // Do you have wings?
@@ -26,26 +26,31 @@ module.exports = {
     if (tryWin(10)) {
       fursonaSpecies += `${content.were}-`;
     }
-    fursonaSpecies += randomItem(SPECIES);
+
+    // Hybrids exists
+    if (isHybrid) {
+      fursonaSpecies += sampleItemsFromArray(contentGlobal.species, randomBetween(2, 3)).join('/');
+      fursonaSpecies += ` ${content.hybrid}`;
+    } else {
+      fursonaSpecies += randomItem(contentGlobal.species);
+    }
+    
     // Maybe you are a taur?
     if (tryWin(3)) {
-      fursonaSpecies += `-${content.taur}`;
+      fursonaSpecies += ` ${content.taur}`;
     }
 
     let whoAmI = randomItem(content.whereICameFrom);
     let past = randomItem(content.edgyPast);
     let goals = randomItem(content.goals).replace('$1', randomItem(contentGlobal.describingFurries));
-    let hobby = sampleItemsFromArray(content.hobbies, 3);
+    let hobby = sampleItemsFromArray(content.hobbies, 4);
     let pictureSeed = pad(randomBetween(1, 99999), 5);
 
     msg.reply(
-      `My name is ${name} the ${title} ${fursonaSpecies}. I'm a ${fursonaSpecies} ${whoAmI}, ${past}. My main color is ${randomItem(COLORS)} with ${randomItem(COLORS)} accents! Some of my hobbies are ${hobby.join(', ')} and ${goals} ${randomItem(contentGlobal.expressions)}`,
+      `My name is ${name} the ${title} ${fursonaSpecies}. I'm a ${fursonaSpecies} ${whoAmI}, ${past}. My main color is ${randomItem(contentGlobal.colors)} with ${randomItem(contentGlobal.colors)} accents! Some of the things I like are ${hobby.join(', ')} and ${goals} ${randomItem(contentGlobal.expressions)}`,
       { 
         files: [`https://thisfursonadoesnotexist.com/v2/jpgs/seed${pictureSeed}.jpg`] 
       }
     );
-    // msg.channel.send('pong');
   },
 };
-
-// https://thisfursonadoesnotexist.com/v2/jpgs/seed99999.jpg
