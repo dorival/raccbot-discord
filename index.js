2261f9c88d67c6fddd91015ab8478135cc97d6f7
Dorival Neto <dorivaltn@gmail.com>
2020-12-20 16:33:57 -0500

    Added support for Dev and Prod switchers

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 require('dotenv').config();
 const Discord = require('discord.js');
+const content = require("./language/en/bot.json");
 const botCommands = require('./commands');
 const bot = new Discord.Client();
 const TOKEN = process.env.TOKEN;
 
 bot.commands = new Discord.Collection();
 Object.keys(botCommands).map(key => {
-  bot.commands.set(botCommands[key].name, botCommands[key]);
+  const namespace = process.env.IS_DEV === 'true' ? 'dev' : '';
+  bot.commands.set(namespace + botCommands[key].name, botCommands[key]);
 });
 
 bot.login(TOKEN);
@@ -30,23 +32,6 @@ bot.on('message', msg => {
 
   } catch (error) {
     console.error(error);
-    msg.reply(`Yikes! I had an error executing this... ${error.message}`);
+    msg.reply(`${content.catchAllErrorMessage} ${error.message}`);
   }
-
-  // if (msg.content === 'ping') {
-  //   const list = bot.users.cache
-  //     .filter(user => !user.bot)
-  //     .map(user => user.username)
-
-  //   // msg.reply(list.concat('\n'));
-  //   msg.channel.send(list.concat('\n'));
-
-  // } else if (msg.content.startsWith('!kick')) {
-  //   if (msg.mentions.users.size) {
-  //     const taggedUser = msg.mentions.users.first();
-  //     msg.channel.send(`You wanted to kick: ${taggedUser.username}`);
-  //   } else {
-  //     msg.reply('Please tag a valid user!');
-  //   }
-  // }
 });
